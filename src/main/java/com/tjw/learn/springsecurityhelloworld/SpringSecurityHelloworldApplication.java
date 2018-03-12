a54ee07114db9b9e2b7b0edef5b8ffe07c7b1011
wb-tjw362524 <wb-tjw362524@alibaba-inc.com>
2018-03-12 11:33:33 +0800

    first

---
+++ src/main/java/com/tjw/learn/springsecurityhelloworld/SpringSecurityHelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.tjw.learn.springsecurityhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityHelloworldApplication.class, args);
+	}
+}
